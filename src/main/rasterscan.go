f8661a112dae8cba4fe7a7d26eaa67cf8d85615e
Lorenzo Lucherini <lorenzo.lucherini@kdm.it>
2014-04-30 17:59:28 +0200

    Cleaned up the code a bit and added command line parameters

--- src/main/rasterscan.go
+++ src/main/rasterscan.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"fmt"
 	"github.com/nfnt/resize"
+	"strconv"
 )
 
 const SIZE int = 1600
@@ -72,57 +73,63 @@ func draw(img *image.RGBA, x, y int, color Color) {
 	img.Set(x, SIZE-1-y, color)
 } 
 
+func distanceEstimator(c complex128, maxIterations int, escapeRadius float64) (float64, float64, float64) {
+	
+	iterations := 1
+			
+	z := c
+	
+	// Used for period detection
+	iterationsFast := 1
+	z_fast := z
+	
+	dz := complex(1,0)
+	
+	// Distance Estimator Algorithm
+	for cmplx.Abs(z) <= escapeRadius && iterations < maxIterations {
+		z2 := z*z + c
+		dz = 2*z*dz + 1
+		z = z2
+		iterations = iterations + 1
+		
+		// Period detection
+		z_fast = z_fast * z_fast + c
+		z_fast = z_fast * z_fast + c
+		iterationsFast = iterationsFast + 1
+		
+		if z == z_fast {
+			//period := iterationsFast - iterations
+			iterations = maxIterations
+		}
+	}
+	
+	magZ := cmplx.Abs(z)
+	distanceEstimate := magZ * math.Log(magZ) / cmplx.Abs(dz)
+	dwell := float64(iterations)
+	
+	if (iterations < maxIterations) {
+		dwell += math.Log2(math.Log2(magZ)) - math.Log2(math.Log2(escapeRadius))
+	}
+
+	return distanceEstimate, dwell, cmplx.Phase(z)
+	
+}
+
 func plot(img *image.RGBA, location *Coordinate, maxIterations int) {
 	for x := 0; x < SIZE; x++ {
 		for y := 0; y < SIZE; y++ {
 			r, i := convert(x, y, location)
 			c := complex(r, i)
 			
-			escapeRadius := 10.0
-			iterations := 1
+			distanceEstimate, D, phase := distanceEstimator(c, maxIterations, 10.0)
 			
-			z := c
-
-			// Used for period detection
-			iterationsFast := 1
-			z_fast := z
-			
-			dz := complex(1,0)
-			
-			// Distance Estimator Algorithm
-			for cmplx.Abs(z) <= escapeRadius && iterations < maxIterations {
-				z2 := z*z + c
-				dz = 2*z*dz + 1
-				z = z2
-				iterations = iterations + 1
-
-				// Period detection
-				z_fast = z_fast * z_fast + c
-				z_fast = z_fast * z_fast + c
-				iterationsFast = iterationsFast + 1
-
-				if z == z_fast {
-					//period := iterationsFast - iterations
-					iterations = maxIterations
-				}
-			}
-			
-			magZ := cmplx.Abs(z)
-			distanceEstimate := magZ * math.Log(magZ) / cmplx.Abs(dz)
-			D := float64(iterations)
-			
-			if (iterations < maxIterations) {
-				D += math.Log2(math.Log2(magZ)) - math.Log2(math.Log2(escapeRadius))
-			}
-
 			dwell := math.Floor(D)
-			finalrad := D - math.Floor(D)
-			finalang := cmplx.Phase(z)
-			
+			finalrad := D - dwell
+	
 			// The Distance Estimate is scaled based on the size of the image and
 			// the magnification factor
 			dscale := math.Log2(distanceEstimate / location.size * (float64(SIZE)/2))
-
+			
 			if int(D) >= maxIterations {
 				draw(img, x, y, White)
 			} else {
@@ -132,6 +139,8 @@ func plot(img *image.RGBA, location *Coordinate, maxIterations int) {
 				// on Robert Munafo's website: http://mrob.com/pub/muency/color.html
 				var value float64
 				
+				// Convert the scaled distance estimate to a brightness (called
+				// "value" in the HSV color space) from 0.0 to 1.0 in 8 intervals.
 				if dscale > 0 {
 					value = 1.0
 				} else if dscale > -8 {
@@ -139,11 +148,20 @@ func plot(img *image.RGBA, location *Coordinate, maxIterations int) {
 				} else {
 					value = 0
 				}
-
+				
+				/*The following re-maps the range 0.0...1.0 onto an "angle"
+				 and "radius" on the color wheel. This color wheel has white in the
+				 center, pastel hues close to the center, and vivid colors around the
+				 edge of the wheel. In order to gain maximum use of all the available
+				 colors, we compute an angle and radius in a way that places the
+				 resulting points equally far apart from one another. The use
+				 of square root means that the radius increases more slowly as we
+				 move away from the center. Note also that the angle will be multiplied
+				 by a constant below, so we'll end up going around the wheel multiple times*/
 				P := math.Log(D)/math.Log(100000)
-
+				
 				var angle, radius float64
-
+				
 				if P<0.5 {
 					P = 1.0 - 1.5*P
 					angle = 1-P
@@ -153,29 +171,34 @@ func plot(img *image.RGBA, location *Coordinate, maxIterations int) {
 					angle = P
 					radius = math.Sqrt(P)
 				}
-
+				
+				// Brighness and saturation are lowered a bit
+				// in every other "stripe".
 				if int(dwell) % 2 != 0 {
 					value = 0.85 * value
 					radius = 0.667 * radius
 				}
-
-				if finalang < 0 {
+				
+				// This breaks the stripe into "squares" to make
+				// external angles evident.
+				if phase < 0 {
 					angle = angle + 0.02
 				}
-
+				
 				angle = angle + 0.0001 * finalrad
 				hue := angle * 5.0
 				hue = hue - math.Floor(hue)
 				// Hue is now in the range [0..1] and needs to be
 				// converted to [0..360[
 				hue *= 359
-
+				
 				saturation := radius - math.Floor(radius)
-
+				
 				draw(img, x, y, Hsv(hue, saturation, value))
 			}
 		}
 	}
+	
 }
 
 func main() {
@@ -187,34 +210,48 @@ func main() {
 
 	img := image.NewRGBA(rect)
 	
-	//plot(img, NewCoordinate(-0.9189, 0.2646, 0.0625), 10000)
-	//plot(img, NewCoordinate(-1.74876455,0,2.4e-5), 10000)
-	//plot(img, NewCoordinate(-1.768620774, 0.002428273, 9e-6), 10000)
-	//plot(img, NewCoordinate(-0.75, 0, 3.5), 1000)
-	//plot(img, NewCoordinate(-1.76733, 0.00002, 0.05), 10000)
-	//plot(img, NewCoordinate(-0.7454291059302, 0.1130087933933, 1.8e-10), 10000)
-	//plot(img, NewCoordinate(-1.7499254344, 0.0000000003, 2.975e-7), 10000)
-	//plot(img, NewCoordinate(-0.744539860355905, 0.121723773894425, 7.5e-12), 10000)
-	//plot(img, NewCoordinate(-0.7469449763063739, 0.10787392545265853, 5e-6), 10000)
-	//plot(img, NewCoordinate(+0.2925755, -0.0149977, +0.0005), 10000)
-	//plot(img, NewCoordinate(-0.74453985651, +0.12172377365, 3.072e-8), 50000)
-	//plot(img, NewCoordinate(-.743643887037151, .131825904205330, .000000000051299), 10000)
-	//plot(img, NewCoordinate(-0.5434, 0.6132, 0.19), 10000)
-	//plot(img, NewCoordinate(-1.2565061941546132, 0.3961765209994203, 2e-4), 10000)
-	plot(img, NewCoordinate(-1.9427191355269593, -3.51354564072135E-4, 1e-12), 10000)
+	usage := fmt.Sprintf("Usage: %v <real> <img> <size>", os.Args[0]) 
 
+	getFloat := func(arg string) float64 {
+		f, err := strconv.ParseFloat(arg, 64)
+		if (err != nil) {
+			fmt.Println(err)
+			fmt.Println(usage)
+			os.Exit(1)
+		}
+
+		return f
+	}
+
+	var r,i,size float64
+	
+	if len(os.Args) < 4 {
+		// Discard arguments and plot the set centered and zoomed out
+		r = -0.75
+		i = 0
+		size = 3.5
+	} else {
+		r = getFloat(os.Args[1])
+		i = getFloat(os.Args[2])
+		size = getFloat(os.Args[3])
+	}
+	
+	plot(img, NewCoordinate(r,i,size), 10000)
+	
+	// The final image is half the size of the generated one.
+	// This way we get a crisp anti-aliased image
 	downscaled := resize.Resize(uint(SIZE/2), 0, img, resize.Lanczos3)
 
 	file, err := os.Create("out.png")
 	
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
-
+		
 	err = png.Encode(file, downscaled)
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
-
 }
